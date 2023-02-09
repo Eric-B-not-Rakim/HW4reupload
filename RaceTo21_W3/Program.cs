9f835ffdd820e11dcae0149f64cb869c8bf04b3f
Eric-B-not-Rakim <baumeister713@gmail.com>
2023-02-08 23:19:13 -0500

    Add project files.

---
+++ RaceTo21_W3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RaceTo21
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            CardTable cardTable = new CardTable();
+            Game game = new Game(cardTable);
+            while (game.nextTask != "GameOver")
+            {
+                game.DoNextTask();
+            }
+        }
+
+        // TRACKING TOTAL POINTS:
+        // Because this keeps track of the overall structure of the game,
+        // I figure I can track total points or high scores for each player here.
+        // Otherwise, players will get reset every game, as will their total scores.
+    }
+}
+
